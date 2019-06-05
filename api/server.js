045e545d9b37d0261d80f49aa4cf7ea25bebc7c0
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-05 14:15:27 +0800

    Remove serverless settings

--- api/server.js
+++ api/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const logger = require('morgan')
-const serverless = require('serverless-http')
 const bcrpyt = require('bcryptjs')
+
+// Import data models
+const db = require('../data/models')
 
 // Instantiate server and router
 const server = express()
@@ -16,6 +18,33 @@ server.use(express.json())
 server.use(logger('dev'))
 
 // Routes
+router.get('/users', async (req, res) => {
+  try {
+    let data = await db.find('users')
+    res.send(data)
+  }
+  catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+router.post('/register', requiredData, async (req, res) => {
+  try {
+
+  }
+  catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+
+  }
+  catch (err) {
+    res.status(500).send(err.message)
+  }
+})
 
 // Activate routes
 server.use('/.netlify/functions/server/api', router)
@@ -23,5 +52,18 @@ server.use('/', (req, res) => {
   res.send(`<h1>WebAuth I Challenge API server</h1>`)
 })
 
+// Custom middleware
+const inputDataChecker = (arr, target) => target.every(v => arr.includes(v))
+const requiredFields = ['name', 'is_complete']
+
+function requiredData(req, res, next) {
+  if (!req.body || !Object.keys(req.body).length) {
+    res.status(400).json({ message: "Missing user data" })
+  } else if (!inputDataChecker(Object.keys(req.body), requiredFields)) {
+    res.status(400).json({ message: "Missing required field." })
+  } else {
+    next()
+  }
+}
+
 module.exports = server
-module.exports.handler = serverless(server)
