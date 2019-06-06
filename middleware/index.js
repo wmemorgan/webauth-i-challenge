5048aabcb4c3a7f57e82ae08bf7cbe39f1f89190
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-06 11:05:21 +0800

    Refactor userAuth middleware to use session data

--- middleware/index.js
+++ middleware/index.js
@@ -47,18 +47,11 @@ async function validateUser(req, res, next) {
   }
 }
 
-async function userAuthorization(req, res, next) {
-  let { username, password } = req.body
-  try {
-    let user = await db.findByUser(username, 'Users')
-    if (user && bcrypt.compareSync(password, user.password)) {
-      next()
-    } else {
-      res.status(401).json({ message: 'You shall not pass!' })
-    }
-  }
-  catch (err) {
-    res.status(500).send(err.message)
+function userAuthorization(req, res, next) {
+  if (req.session && req.session.user) {
+    next()
+  } else {
+    res.status(401).json({ message: 'You shall not pass!' })
   }
 }
 
